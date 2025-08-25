4fe42c9a1fcf570ffda2a5a8e8c0d8c8042925d8
matthieu <matthieu@les-mnms.net>
2025-08-25 14:20:33 +0000

    UTF8 conversion

--- modules/admin/updcron.inc.php
+++ modules/admin/updcron.inc.php
@@ -1,9 +1,9 @@
 <?
-// ---- Refuse l'acc�s en direct
+// ---- Refuse l'accès en direct
 	if ((!isset($token)) || ($token==""))
 	  { header("HTTP/1.0 401 Unauthorized"); exit; }
 
-// ---- V�rifie les param�tres
+// ---- Vérifie les paramètres
 	$res=array();
 	if (!isset($_GET["id"]))
 	{
